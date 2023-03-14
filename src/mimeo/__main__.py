6e186c0fe93e8c9c89cdc39b4bef7e21c30b5231
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-03-14 14:36:23 +0100

    mimeo #11 - allow for all integers in indent and validate config after customization

--- src/mimeo/__main__.py
+++ src/mimeo/__main__.py
@@ -11,7 +11,7 @@ class MimeoArgumentParser(ArgumentParser):
         super().__init__(prog="mimeo", description="Generate data based on a simple template")
         self.add_argument("paths", nargs="+", type=str, help="take paths to Mimeo Configurations")
         self.add_argument("-x", "--xml-declaration", type=str, choices=["true", "false"])
-        self.add_argument("-i", "--indent", type=int, choices=[0, 2, 4])
+        self.add_argument("-i", "--indent", type=int)
         self.add_argument("-v", "--version", action="version", version="%(prog)s v1.0.2")
 
 
@@ -19,19 +19,17 @@ def main():
     mimeo_parser = MimeoArgumentParser()
     args = mimeo_parser.parse_args()
     for path in args.paths:
-        mimeo_config = get_config(path)
-        if args.xml_declaration is not None:
-            mimeo_config.xml_declaration = args.xml_declaration.lower() == "true"
-        if args.indent is not None:
-            if args.indent >= 0:
-                mimeo_config.indent = args.indent
-
+        mimeo_config = get_config(path, args)
         Mimeograph(mimeo_config).produce()
 
 
-def get_config(config_path):
+def get_config(config_path, args):
     with open(config_path) as config_file:
         config = json.load(config_file)
+        if args.xml_declaration is not None:
+            config[MimeoConfig.XML_DECLARATION_KEY] = args.xml_declaration.lower() == "true"
+        if args.indent is not None:
+            config[MimeoConfig.INDENT_KEY] = args.indent
     return MimeoConfig(config)
 
 
