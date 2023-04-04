f508901ac64caafa3cbd2eee50ee48bc89887a12
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-04-04 08:07:47 +0200

    mimeo #56 - rename utils to tools

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,16 +1,16 @@
 import pytest
 
-from mimeo import utils
+from mimeo import tools
 from mimeo.exceptions import ResourceNotFound
 
 
 def test_get_resource_existing():
-    with utils.get_resource("logging.yaml") as resource:
+    with tools.get_resource("logging.yaml") as resource:
         assert resource.name.endswith("resources/logging.yaml")
 
 
 def test_get_resource_non_existing():
     with pytest.raises(ResourceNotFound) as err:
-        utils.get_resource("non-existing-file.yaml")
+        tools.get_resource("non-existing-file.yaml")
 
     assert err.value.args[0] == "No such resource: [non-existing-file.yaml]"
