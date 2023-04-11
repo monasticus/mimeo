d371da51763a15188de3a99d5931806a7c79e1cb
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-04-11 11:01:13 +0200

    mimeo #56 - create mimeo context manager

---
+++ src/mimeo/context/mimeo_context_manager.py
@@ -0,0 +1,40 @@
+from mimeo.config import MimeoConfig
+from mimeo.context import MimeoContext
+from mimeo.meta import Alive, OnlyOneAlive
+
+
+class MimeoContextManager(Alive, metaclass=OnlyOneAlive):
+
+    __INSTANCES = []
+
+    def __init__(self, mimeo_config: MimeoConfig = None):
+        super().__init__()
+        self.__mimeo_config = mimeo_config
+        self.__vars = {}
+        self.__contexts = {}
+        self.__current_context = None
+
+    def __enter__(self):
+        super().__enter__()
+        self.__vars = self.__mimeo_config.vars
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        super().__exit__(exc_type, exc_val, exc_tb)
+        self.__vars = None
+        self.__contexts = None
+        return None
+
+    def get_context(self, context: str) -> MimeoContext:
+        super().assert_alive()
+        if context not in self.__contexts:
+            self.__contexts[context] = MimeoContext(context)
+        return self.__contexts[context]
+
+    def get_current_context(self) -> MimeoContext:
+        super().assert_alive()
+        return self.__current_context
+
+    def set_current_context(self, context: MimeoContext) -> None:
+        super().assert_alive()
+        self.__current_context = context
---
+++ tests/context/test_mimeo_context_manager.py
@@ -0,0 +1,63 @@
+import pytest
+
+from mimeo.config import MimeoConfig
+from mimeo.context import MimeoContextManager
+from mimeo.meta.exc import InstanceNotAlive
+
+
+@pytest.fixture(autouse=True)
+def default_config():
+    return MimeoConfig({
+        "_templates_": [
+            {
+                "count": 10,
+                "model": {
+                    "SomeEntity": {
+                        "ChildNode1": 1,
+                        "ChildNode2": "value-2",
+                        "ChildNode3": True
+                    }
+                }
+            }
+        ]
+    })
+
+
+def test_get_context_not_initialized(default_config):
+    mimeo_manager = MimeoContextManager(default_config)
+    with pytest.raises(InstanceNotAlive) as err:
+        mimeo_manager.get_context("SomeContext")
+
+    assert err.value.args[0] == "The instance is not alive!"
+
+    with MimeoContextManager(default_config) as mimeo_manager:
+        mimeo_manager.get_context("SomeContext")
+
+    with pytest.raises(InstanceNotAlive) as err:
+        mimeo_manager.get_context("SomeContext")
+
+    assert err.value.args[0] == "The instance is not alive!"
+
+
+def test_get_context(default_config):
+    with MimeoContextManager(default_config) as mimeo_manager:
+        context = mimeo_manager.get_context("SomeContext")
+        assert context.name == "SomeContext"
+        assert context.curr_id() == 0
+
+        context2 = mimeo_manager.get_context("SomeContext")
+        assert context is context2
+
+        mimeo_manager2 = MimeoContextManager()
+        context3 = mimeo_manager2.get_context("SomeContext")
+        assert context is context3
+
+
+def test_current_context(default_config):
+    with MimeoContextManager(default_config) as mimeo_manager:
+        assert mimeo_manager.get_current_context() is None
+
+        context = mimeo_manager.get_context("SomeContext")
+        mimeo_manager.set_current_context(context)
+
+        assert mimeo_manager.get_current_context() is context
