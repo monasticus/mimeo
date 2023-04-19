1ac018048f420f4c3b0b6dacdae327c1c548211c
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-04-19 10:16:53 +0200

    mimeo #33 - add documentation to the config package

--- src/mimeo/config/__init__.py
+++ src/mimeo/config/__init__.py
@@ -1,1 +1,13 @@
+"""
+The config package contains python classes
+representing Mimeo Configuration components
+
+It exports just one class
+- MimeoConfig: A python representation of the Mimeo Configuration
+
+To use this package, simply import it:
+    from mimeo.config import MimeoConfig
+
+"""
+
 from .mimeo_config import MimeoConfig
