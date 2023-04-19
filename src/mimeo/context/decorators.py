34d3c05987d32a644c97d6899ddf372a392f51ee
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-04-19 10:42:13 +0200

    mimeo #33 - add documentation to the decorators module

--- src/mimeo/context/decorators.py
+++ src/mimeo/context/decorators.py
@@ -1,10 +1,47 @@
+"""
+Decorators for the context package.
+
+This module defines the following decorators:
+
+- @mimeo_context:
+    Decorator providing a 'context' parameter to a function
+- @mimeo_context_switch:
+    Decorator switching context before and after function's call
+- @mimeo_next_iteration:
+    Decorator incrementing current context's iteration
+- @mimeo_clear_iterations:
+    Decorator clearing iterations of the current context
+
+"""
+
 import functools
+from typing import Callable
 
 from mimeo.config.mimeo_config import MimeoTemplate
 from mimeo.context import MimeoContext, MimeoContextManager
 
 
-def mimeo_context(func):
+def mimeo_context(func: Callable) -> Callable:
+    """
+    Decorator providing a 'context' parameter to a function.
+
+    It can be used only for functions having defined a 'context'
+    parameter. In case the function being decorated is called providing
+    this param, decorator simply calls it without any modification.
+    Otherwise - and this is the main purpose of the @mimeo_context
+    decorator - it provides the current Mimeo Context.
+
+    Parameters
+    ----------
+    func : Callable
+        The function being decorated
+
+    Returns
+    -------
+    inject_context : Callable
+        The decorated function
+    """
+
     @functools.wraps(func)
     def inject_context(*args, **kwargs):
         if any(isinstance(arg, MimeoContext) for arg in args) or "context" in kwargs:
@@ -16,7 +53,28 @@ def mimeo_context(func):
     return inject_context
 
 
-def mimeo_context_switch(func):
+def mimeo_context_switch(func: Callable) -> Callable:
+    """
+    Decorator switching context before and after function's call.
+
+    It can be used only for functions having defined a MimeoTemplate
+    parameter. It switches a Mimeo Context to the one defined in
+    Mimeo Template, and after the function's being decorated call
+    switches back to the previous one. It helps to handle nested
+    Mimeo Templates. This decorator meant to be used for
+    Generator's function that generates data from a template.
+
+    Parameters
+    ----------
+    func : Callable
+        The function being decorated
+
+    Returns
+    -------
+    switch_context : Callable
+        The decorated function
+    """
+
     @functools.wraps(func)
     def switch_context(*args, **kwargs):
         context_mng = MimeoContextManager()
@@ -38,6 +96,23 @@ def mimeo_context_switch(func):
 
 
 def mimeo_next_iteration(func):
+    """
+    Decorator incrementing current context's iteration.
+
+    It is meant to be used for Generator's function that
+    generates a single data copy.
+
+    Parameters
+    ----------
+    func : Callable
+        The function being decorated
+
+    Returns
+    -------
+    next_iteration : Callable
+        The decorated function
+    """
+
     @functools.wraps(func)
     def next_iteration(*args, **kwargs):
         MimeoContextManager().get_current_context().next_iteration()
@@ -48,6 +123,23 @@ def mimeo_next_iteration(func):
 
 
 def mimeo_clear_iterations(func):
+    """
+    Decorator clearing iterations of the current context.
+
+    It is meant to be used for Generator's function that generates
+    data from a template.
+
+    Parameters
+    ----------
+    func : Callable
+        The function being decorated
+
+    Returns
+    -------
+    clear_iterations : Callable
+        The decorated function
+    """
+
     @functools.wraps(func)
     def clear_iterations(*args, **kwargs):
         MimeoContextManager().get_current_context().clear_iterations()
