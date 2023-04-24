4d2f43dc41b56aba25b119bd295624cee7c59601
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-04-24 08:03:05 +0200

    mimeo #27 - create the LastNamesDB class and fix first names source file name

--- tests/database/test_first_names_db.py
+++ tests/database/test_first_names_db.py
@@ -16,7 +16,7 @@ def test_get_first_names():
 
 
 def test_get_first_name_at():
-    with open("src/mimeo/resources/firstnames.csv", "r") as first_names:
+    with open("src/mimeo/resources/forenames.csv", "r") as first_names:
         headers = next(first_names)
         first_name_1_cols = next(first_names).rstrip().split(",")
         first_name_2_cols = next(first_names).rstrip().split(",")
---
+++ tests/database/test_last_names_db.py
@@ -0,0 +1,38 @@
+import pytest
+
+from mimeo.database import LastNamesDB
+from mimeo.database.exc import InvalidIndex
+
+
+def test_get_last_names():
+    last_names = LastNamesDB.get_last_names()
+    assert len(last_names) == LastNamesDB.NUM_OF_RECORDS
+
+    last_names.pop(0)
+    assert len(last_names) == LastNamesDB.NUM_OF_RECORDS - 1
+
+    last_names = LastNamesDB.get_last_names()
+    assert len(last_names) == LastNamesDB.NUM_OF_RECORDS
+
+
+def test_get_last_name_at():
+    with open("src/mimeo/resources/surnames.txt", "r") as last_names:
+        last_name_1_source = next(last_names).rstrip()
+        last_name_2_source = next(last_names).rstrip()
+
+    db = LastNamesDB()
+
+    last_name_1 = db.get_last_name_at(0)
+    assert last_name_1 == last_name_1_source
+
+    last_name_2 = db.get_last_name_at(1)
+    assert last_name_2 == last_name_2_source
+
+
+def test_get_last_name_at_out_of_range():
+    db = LastNamesDB()
+
+    with pytest.raises(InvalidIndex) as err:
+        db.get_last_name_at(151670)
+
+    assert err.value.args[0] == "Provided index [151670] is out or the range: 0-151669!"
