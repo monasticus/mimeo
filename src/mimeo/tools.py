5d8766bfe5857f43d8a4b35f7591fddd7f43f45d
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-04-29 07:40:38 +0200

    mimeo #33 - add documentation to resources exceptions module

--- src/mimeo/tools.py
+++ src/mimeo/tools.py
@@ -14,4 +14,4 @@ def get_resource(resource_name: str) -> TextIO:
     try:
         return pkg_resources.open_text(data, resource_name)
     except FileNotFoundError:
-        raise ResourceNotFound(f"No such resource: [{resource_name}]")
+        raise ResourceNotFound(resource_name)
