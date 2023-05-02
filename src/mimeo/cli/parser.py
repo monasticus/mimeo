8c9464e4f3b21ffe40c24e447abbc2d5e16f5070
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-05-02 14:17:48 +0200

    mimeo #33 - add documentation to the mimeo parser module

--- src/mimeo/cli/parser.py
+++ src/mimeo/cli/parser.py
@@ -1,13 +1,79 @@
+"""The Mimeo Parser module.
+
+It exports just a single class:
+    * MimeoArgumentParser
+        A custom ArgumentParser for the Mimeo CLI.
+"""
 from argparse import ArgumentParser
 
 
 class MimeoArgumentParser(ArgumentParser):
+    """A custom ArgumentParser for the Mimeo CLI."""
 
     def __init__(self):
+        """Initialize MimeoArgumentParser class.
+
+        Extends ArgumentParser constructor with a Mimeo CLI details.
+
+        It provides the following command line interface:
+
+        usage: mimeo [OPTIONS] paths
+
+        Generate data based on a template
+
+        positional arguments:
+          paths                 take paths to Mimeo Configuration files
+
+        optional arguments:
+          -h, --help            show this help message and exit
+          -v, --version         show program's version number and exit
+
+        Mimeo Configuration arguments:
+          -x {true,false}, --xml-declaration {true,false}
+                                overwrite the xml_declaration property
+          -i INDENT, --indent INDENT
+                                overwrite the indent property
+          -o {file,stdout,http}, --output {file,stdout,http}
+                                overwrite the output_details/direction property
+          -d DIRECTORY_PATH, --directory DIRECTORY_PATH
+                                overwrite the output_details/directory_path property
+          -f FILE_NAME, --file FILE_NAME
+                                overwrite the output_details/file_name property
+          -H HOST, --http-host HOST
+                                overwrite the output_details/host property
+          -p PORT, --http-port PORT
+                                overwrite the output_details/port property
+          -E ENDPOINT, --http-endpoint ENDPOINT
+                                overwrite the output_details/endpoint property
+          -U USERNAME, --http-user USERNAME
+                                overwrite the output_details/username property
+          -P PASSWORD, --http-password PASSWORD
+                                overwrite the output_details/password property
+          --http-method METHOD
+                                overwrite the output_details/method property
+          --http-protocol PROTOCOL
+                                overwrite the output_details/protocol property
+          --http-auth AUTH
+                                overwrite the output_details/auth property
+          -e ENVIRONMENT, --http-env ENVIRONMENT
+                                overwrite the output_details http properties using a mimeo environment configuration
+          --http-envs-file PATH
+                                use a custom environments file (by default: mimeo.envs.json)
+
+        Logging arguments:
+          --silent              disable INFO logs
+          --debug               enable DEBUG mode
+          --fine                enable FINE mode
+        """
         super().__init__(
             prog="mimeo",
             description="Generate data based on a template",
             usage="%(prog)s [OPTIONS] paths")
+        self._add_positional_arguments()
+        self._add_mimeo_configuration_arguments()
+        self._add_logging_arguments()
+
+    def _add_positional_arguments(self):
         self.add_argument(
             "-v",
             "--version",
@@ -19,6 +85,7 @@ class MimeoArgumentParser(ArgumentParser):
             type=str,
             help="take paths to Mimeo Configuration files")
 
+    def _add_mimeo_configuration_arguments(self):
         mimeo_config_args = self.add_argument_group("Mimeo Configuration arguments")
         mimeo_config_args.add_argument(
             "-x",
@@ -106,6 +173,7 @@ class MimeoArgumentParser(ArgumentParser):
             metavar="PATH",
             help=f"use a custom environments file (by default: mimeo.envs.json)")
 
+    def _add_logging_arguments(self):
         logging_args = self.add_argument_group("Logging arguments")
         logging_args_excl = logging_args.add_mutually_exclusive_group()
         logging_args_excl.add_argument(
