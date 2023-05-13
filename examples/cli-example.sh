323d19062a285f621c869dc0920fbe0c8fdf68d5
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-05-13 07:48:30 +0200

    mimeo #66 - add cli examples

--- examples/cli-example.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mimeo examples/1-introduction/01-basic.json
---
+++ examples/cli-examples.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Simple
+mimeo examples/1-introduction/01-basic.json
+
+# Custom output
+mimeo examples/1-introduction/01-basic.json -o stdout
+
+# Custom directory and file name
+mimeo examples/1-introduction/01-basic.json -d basic-data -f some-entity
+
+# Customized xml declaration and indent
+mimeo examples/1-introduction/01-basic.json -x true -i 4
+
+# HTTP customization
+mimeo examples/1-introduction/01-basic.json -o http -H localhost -p 8080 -E /data -U admin -P admin
+mimeo examples/1-introduction/01-basic.json -o http -e local
+mimeo examples/1-introduction/01-basic.json -o http -e local --http-envs-file config/environments.json
+mimeo examples/1-introduction/01-basic.json -o http -e local -E /v2/data
+mimeo examples/1-introduction/01-basic.json -o http -e local -U custom-user -P custom-password
