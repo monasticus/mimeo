a523cf7c954c7869fc1289590e9213f2d941273c
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-05-17 15:15:00 +0200

    mimeo #119 - validate random_str params

--- tests/utils/test_random_str_util.py
+++ tests/utils/test_random_str_util.py
@@ -1,4 +1,6 @@
+from mimeo.utils.exc import InvalidValueError
 from mimeo.utils.renderers import UtilsRenderer
+from tests.utils import assert_throws
 
 
 def test_random_str_raw():
@@ -19,3 +21,19 @@ def test_random_str_parametrized_custom():
     random_str = UtilsRenderer.render_parametrized(mimeo_util)
     assert isinstance(random_str, str)
     assert len(random_str) == 3
+
+
+def test_random_str_parametrized_with_length_zero():
+    mimeo_util = {"_name": "random_str", "length": 0}
+    random_str = UtilsRenderer.render_parametrized(mimeo_util)
+    assert isinstance(random_str, str)
+    assert random_str == ""
+
+
+@assert_throws(err_type=InvalidValueError,
+               msg=("The random_str Mimeo Util cannot be parametrized with negative "
+                    "length [{length}] value"),
+               params={"length": -1})
+def test_random_str_parametrized_with_negative_length():
+    mimeo_util = {"_name": "random_str", "length": -1}
+    UtilsRenderer.render_parametrized(mimeo_util)
