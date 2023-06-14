b99a0033ac70dd74384a23e3ce7dac784e6cfea9
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-06-14 16:15:23 +0200

    mimeo #21 - add a unit test for mimeograph

--- tests/test_mimeograph.py
+++ tests/test_mimeograph.py
@@ -16,7 +16,7 @@ def _teardown():
 
 
 @pytest.mark.asyncio()
-async def test_produce():
+async def test_produce_xml():
     config = {
         "output": {
             "direction": "file",
@@ -57,6 +57,51 @@ async def test_produce():
                 assert file.readline() == "    <ChildNode2>value-2</ChildNode2>\n"
                 assert file.readline() == "    <ChildNode3>true</ChildNode3>\n"
                 assert file.readline() == "</SomeEntity>\n"
+
+
+@pytest.mark.asyncio()
+async def test_produce_json():
+    config = {
+        "output": {
+            "direction": "file",
+            "format": "json",
+            "indent": 4,
+            "xml_declaration": True,
+            "directory_path": "test_mimeograph-dir",
+            "file_name": "output",
+        },
+        "_templates_": [
+            {
+                "count": 10,
+                "model": {
+                    "SomeEntity": {
+                        "ChildNode1": 1,
+                        "ChildNode2": "value-2",
+                        "ChildNode3": True,
+                    },
+                },
+            },
+        ],
+    }
+    mimeo_config = MimeoConfigFactory.parse(config)
+    with MimeoContextManager(mimeo_config):
+        mimeo = Mimeograph(mimeo_config)
+
+        assert not Path("test_mimeograph-dir").exists()
+        await mimeo.process()
+        assert Path("test_mimeograph-dir").exists()
+        for i in range(1, 11):
+            file_path = f"test_mimeograph-dir/output-{i}.json"
+            assert Path(file_path).exists()
+
+            with Path(file_path).open() as file:
+                assert file.readline() == '{\n'
+                assert file.readline() == '    "SomeEntity": {\n'
+                assert file.readline() == '        "ChildNode1": 1,\n'
+                assert file.readline() == '        "ChildNode2": "value-2",\n'
+                assert file.readline() == '        "ChildNode3": true\n'
+                assert file.readline() == '    }\n'
+                assert file.readline() == '}'
 
 
 
