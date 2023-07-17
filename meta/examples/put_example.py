f3ce843a5d1561851b82b153bc188603fa28f971
Tomasz Aniołowski <tomasz.maciej.aniolowski@gmail.com>
2023-07-17 10:06:55 +0200

    mimeo #167 - prepare the put_example script

---
+++ meta/examples/put_example.py
@@ -0,0 +1,71 @@
+#!venv/bin/python3
+from argparse import ArgumentParser
+from pathlib import Path
+
+FILE_DELIMITER = "-"
+
+
+class PutExampleParser(ArgumentParser):
+
+    def __init__(self):
+        super().__init__()
+        self.add_argument(
+            "-p",
+            "--position",
+            nargs="+",
+            type=int,
+            required=True,
+            help="take positions to move examples forward")
+        self.add_argument(
+            "-d",
+            "--directory",
+            type=str,
+            required=True,
+            help="take a directory of examples to move")
+
+
+def main():
+    parser = PutExampleParser()
+    args = parser.parse_args()
+    positions = _get_sorted_and_unique_positions(args.position)
+    for position in positions:
+        with Path(args.directory) as examples_dir:
+            for file in _get_sorted_files(examples_dir):
+                file_name_components = file.name.split(FILE_DELIMITER)
+                example_num = int(file_name_components[0])
+                if example_num < position:
+                    break
+                new_file_name = _get_new_file_name(file_name_components)
+                new_file_path = Path(file).with_name(new_file_name)
+                print(f"{file} -> {new_file_path}")
+                Path(file).rename(new_file_path)
+
+
+def _get_sorted_and_unique_positions(
+        positions: list
+) -> list:
+    positions = [*set(positions)]
+    positions.sort()
+    return positions
+
+
+def _get_sorted_files(
+        path: Path
+) -> list:
+    files = list(path.iterdir())
+    files.sort(reverse=True)
+    return files
+
+
+def _get_new_file_name(
+        file_name_components: list[str]
+) -> str:
+    example_num = int(file_name_components[0])
+    new_example_num = example_num + 1
+    new_file_name_components = [f"{new_example_num:02d}", *file_name_components[1:]]
+    new_file_name = FILE_DELIMITER.join(new_file_name_components)
+    return new_file_name
+
+
+if __name__ == "__main__":
+    main()
